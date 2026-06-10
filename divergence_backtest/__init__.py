4cb8c67a00d2104524b8a534d76547ada602dd5b
GolomtArts <istarvol@proton.me>
2026-06-11 02:11:06 +0800

    Create __init__.py

---
+++ divergence_backtest/__init__.py
@@ -0,0 +1,6 @@
+"""Causal spot/perpetual divergence research backtester."""
+
+from .engine import BacktestConfig, BacktestResult, run_backtest
+
+__all__ = ["BacktestConfig", "BacktestResult", "run_backtest"]
+
